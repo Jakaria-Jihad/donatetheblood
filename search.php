4920956788ecc6e2def7137d2df9fc489f8716d9
Jakaria-Jihad <jakariahossen669@gmail.com>
2022-04-21 02:29:02 +0600

    Modified search options and done.

--- search.php
+++ search.php
@@ -90,7 +90,47 @@
 <div class="container" style="padding: 60px 0 60px 0;">
 	<div class="row " id="data">
 
-		<!-- Display The Search Result -->
+	<?php
+		if((isset($_GET['city']) && !empty($_GET['city'])) && (isset($_GET['blood_group']) && !empty($_GET['blood_group']))){
+
+			$city = $_GET['city'];
+			$blood_group = $_GET['blood_group'];
+			$sql = "SELECT * FROM donor WHERE city = '$city' OR blood_group = '$blood_group'";
+			$result = mysqli_query($connection, $sql);
+			if(mysqli_num_rows($result)>0){
+				while($row = mysqli_fetch_assoc($result)){
+					if($row['save_life_date']==0){
+						echo '<div class ="col-md-3 col-sm-12 col-lg-3 donors_data">
+						<span class = "name">'.$row['name'].'</span>
+						<span>'.$row['gender'].'</span>
+						<span>'.$row['blood_group'].'</span>
+						<span>'.$row['city'].'</span>
+						<span>'.$row['contact_no'].'</span>
+						<span>'.$row['email'].'</span>
+						</div>';
+
+
+					}else{
+						echo '<div class ="col-md-3 col-sm-12 col-lg-3 donors_data">
+						<span class = "name">'.$row['name'].'</span>
+						<span>'.$row['gender'].'</span>
+						<span>'.$row['blood_group'].'</span>
+						<span>'.$row['city'].'</span>
+						<h4 class="name text-center">Donated</h4>
+						</div>';
+					}
+				}
+			}else{
+				echo '<div class="alert alert-success alert-dismissible fade show" role="alert">
+				<strong>Data not found!</strong>
+				<button type="button" class="close" data-dismiss="alert" aria-label="Close">
+				  <span aria-hidden="true">&times;</span>
+				</button>
+			  </div>';
+			}
+		}
+
+	?>
 
 </div>
 </div>
@@ -103,3 +143,25 @@
 	include ('include/footer.php');
 
 ?>
+<script type="text/javascript">
+	$(function(){
+
+		$("#search").on('click', function(){
+			var city = $("#city").val();
+			var city = $("#blood_group").val();
+
+
+			$.ajax({
+				type :'GET',
+				url : 'ajaxsearch.php',
+				data : {city:city, blood_group,blood_group},
+				success : function(data){
+					if(!data.error()){
+						$("#data").html(data);
+					}
+				}
+			});
+		});
+	});
+
+</script>
